6c7dbbc46135873eb560dfc070df5cd57c47b23a
Vadim Semenov <semenov@comfortlab.ru>
2023-02-03 23:47:47 +0300

    Add MineSweeper

---
+++ Minesweeper.Logic/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Minesweeper.Logic;
+
+public class Board
+{
+    public Cell[,] Field { get; set; }
+    public int MineAmount { get; private set; }
+
+    public Board(int rowsAmount, int columnAmount, int mineAmount )
+    {
+        Field = new Cell[rowsAmount,columnAmount];
+
+        MineAmount = mineAmount;
+
+        // InsertMines() 
+    }
+
+}
+
+public record Cell
+{
+    public CellStatus Status { get; set;}
+
+}
+
+public enum CellStatus
+{
+    EmptyCell = 0,
+
+}
